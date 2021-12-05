7b64313b7e622c84ab2415d7fb9b48603402f5b0
Robert Finkley <me@robertfinkley.com>
2021-12-04 20:39:54 -0500

    createdd events api for backend

--- api/events/controllers/events.js
+++ api/events/controllers/events.js
@@ -10,6 +10,54 @@ const { default: createStrapi } = require('strapi');
 
 module.exports = {
   // Get logged in users
+  async create(ctx) {
+    let entity;
+    if (ctx.is('multipart')) {
+      const { data, files } = parseMultipartData(ctx);
+      data.user = ctx.state.user.id;
+      entity = await strapi.services.events.create(data, { files });
+    } else {
+      ctx.request.body.user = ctx.state.user.id;
+      entity = await strapi.services.events.create(ctx.request.body);
+    }
+    return sanitizeEntity(entity, { model: strapi.models.events });
+  },
+  async update(ctx) {
+    const { id } = ctx.params;
+    let entity;
+    const [events] = await strapi.services.events.find({
+      id: ctx.params.id,
+      'user.id': ctx.state.user.id,
+    });
+
+    if (!events) {
+      return ctx.unauthorized(`You can't update this entry`);
+    }
+
+    if (ctx.is('multipart')) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.events.update({ id }, data, {
+        files,
+      });
+    } else {
+      entity = await strapi.services.events.update({ id }, ctx.request.body);
+    }
+
+    return sanitizeEntity(entity, { model: strapi.models.events });
+  },
+  async delete(ctx) {
+    const { id } = ctx.params;
+
+    const [events] = await strapi.services.events.find({
+      id: ctx.params.id,
+      'user.id': ctx.state.user.id,
+    });
+    if (!events) {
+      return ctx.unauthorized(`You can't update this entry`);
+    }
+    const entity = await strapi.services.events.delete({ id });
+    return sanitizeEntity(entity, { model: strapi.models.events });
+  },
   async me(ctx) {
     const user = ctx.state.user;
     if (!user) {
